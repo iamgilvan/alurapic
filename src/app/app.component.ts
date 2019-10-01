2b67e0675ee980bbd3e6bc86307e63c5de355d1a
Gilvan Almeida <g6almeida@hotmail.com>
2019-10-01 19:17:01 -0300

    Criando e declarando o componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alura pic';
-  description ='Leão';
-  url='https://cdn.vox-cdn.com/thumbor/8VEWHAcAt3fZiWZ5knCOmkUsbCE=/0x0:4096x2160/920x613/filters:focal(1416x380:2070x1034):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/64749197/The_Lion_King_dt1_still_1__1_.0.jpg';
 }
---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent{
+    description ='Leão';
+    url='https://cdn.vox-cdn.com/thumbor/8VEWHAcAt3fZiWZ5knCOmkUsbCE=/0x0:4096x2160/920x613/filters:focal(1416x380:2070x1034):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/64749197/The_Lion_King_dt1_still_1__1_.0.jpg';
+}
