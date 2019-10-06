409b5b26df1b97f7e615c404e3c5cd0977af0b75
Gilvan Almeida <g6almeida@hotmail.com>
2019-10-06 20:21:59 -0300

    Consumindo API.

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -7,8 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  photos = [];
+  photos: Object[] = [];
 
-  constructor(http: HttpClient) {
-    console.log(http);
-}}
+  constructor(http: HttpClient) 
+  {
+    http.get<Object[]>('http://localhost:3000/flavio/photos')
+        .subscribe(photos => this.photos = photos);
+  }
+}
