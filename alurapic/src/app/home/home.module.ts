eb35df782bc504202c7b05e98b7a069fd54c28d9
Gilvan Almeida <g6almeida@hotmail.com>
2019-11-21 20:32:00 -0300

    Componetizando mensagem de validação.

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: 
@@ -10,7 +11,8 @@ import { CommonModule } from '@angular/common';
     ],
     imports: [ 
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        VmessageModule
      ]
 
 })
