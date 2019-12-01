e8a6d984a8163f35340ad889409bc2bb5c3f5cff
Gilvan Almeida <g6almeida@hotmail.com>
2019-12-01 19:08:17 -0300

    Guarda rotas.

--- alurapic/src/app/core/user/user.service.ts
+++ alurapic/src/app/core/user/user.service.ts
@@ -10,16 +10,17 @@ import * as jwt_decode from 'jwt-decode'
 export class UserService {
 
   private userSubject = new BehaviorSubject<User>(null);
+  private userName: string
     
-    constructor(private tokenService: TokenService) {
+  constructor(private tokenService: TokenService) {
       
-      this.tokenService.hasToken() &&
-        this.decodeAndNotify();
-     }
-
-    setToken(token: string) {
-      this.tokenService.setToken(token);
+    this.tokenService.hasToken() &&
       this.decodeAndNotify();
+  }
+
+  setToken(token: string) {
+    this.tokenService.setToken(token);
+    this.decodeAndNotify();
   }
 
   getUser() {
@@ -29,11 +30,20 @@ export class UserService {
   private decodeAndNotify() {
       const token = this.tokenService.getToken();
       const user = jwt_decode(token) as User; //<- (aqui a chamada jwt_decode)
+      this.userName = user.name;
       this.userSubject.next(user);
   }
-  
+
   logout() {
     this.tokenService.removeToken();
     this.userSubject.next(null);
-}
+  }
+
+  isLogged() {
+    return this.tokenService.hasToken();
+  }
+
+  getUserName() {
+    return this.userName;
+  }
 }
