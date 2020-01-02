9718340a150562b81d2e3c3c68a1735b7b45dc06
Gilvan Almeida <gilvan.praxedes@hotmail.com>
2020-01-02 18:44:17 -0300

    Upload de foto.

--- alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { VmessageModule } from './../../shared/components/vmessage/vmessage.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
@@ -11,7 +12,8 @@ import { FormsModule } from '@angular/forms';
             CommonModule,
             ReactiveFormsModule,
             VmessageModule,
-            FormsModule
+            FormsModule,
+            RouterModule
         ]
     })
 export class PhotoFormModule { }
