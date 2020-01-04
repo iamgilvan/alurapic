4b9d075bc34574decadc774b83c4562bb2f8a0f7
Gilvan Almeida <gilvan.praxedes@hotmail.com>
2020-01-04 12:14:30 -0300

    Bloqueando acesso não autenticado.

--- alurapic/src/app/core/auth/login.guard.ts
+++ alurapic/src/app/core/auth/login.guard.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { UserService } from '../user/user.service';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
-@Injectable({ providedIn: 'root'})
-export class AuthGuard implements CanActivate {
+@Injectable({ providedIn: 'root' })
+export class LoginGuard {
 
     constructor(
         private userService: UserService,
         private router: Router) {}
-
+    
     canActivate(
-        route: ActivatedRouteSnapshot, 
+        route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-
-            if(!this.userService.isLogged()){
-                this.router.navigate(['']);
+    
+            if(this.userService.isLogged()){
+                this.router.navigate(['user', this.userService.getUserName()])
                 return false;
             }
             return true;
