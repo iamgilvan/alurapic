d80625c1a7a6bcf98690fa26e8372d2d860cb62c
Gilvan Almeida <gilvan.praxedes@hotmail.com>
2020-01-08 18:54:08 -0300

    Camada de serviço do compomente de alerta.

--- alurapic/src/app/shared/alert/alert.service.ts
+++ alurapic/src/app/shared/alert/alert.service.ts
@@ -1,10 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Alert, AlertType } from './alert';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertService {
 
-constructor() { }
+    alertSubject: Subject<Alert>;
+    
+    private alert(alertType: AlertType, message: string) {
+
+      this.alertSubject.next(new Alert(alertType, message))
+    }
+
+    success(message: string) {
+        this.alert(AlertType.SUCCESS, message);
+    }
+
+    warning(message: string) {
+        this.alert(AlertType.WARNING, message);
+    }
+
+    danger(message: string) {
+        this.alert(AlertType.DANGER, message);
+    }
+
+
+    info(message: string) {
+        this.alert(AlertType.INFO, message);
+    }
+
+    getAlert() {
+        return this.alertSubject.asObservable();
+  }
 
 }
